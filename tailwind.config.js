f9bcb1903b5736774d3922fcdf8c29a58ea95c19
Nipa <akternipa941@gmail.com>
2022-10-23 18:42:50 +0600

    custom theme add

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 
+// module.exports = {
+//   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   daisyui: {
+//     themes: ["light"],
+//   },
+//   plugins: [require("daisyui")],
+// };
+
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
-};
+
+  daisyui: {
+    themes: [
+      {
+        skillnaotheme: {
+          primary: "#1A6241",
+          secondary: "#FFB357",
+        },
+      },
+      "light",
+      "cupcake",
+    ],
+  },
+
+  plugins: [require("daisyui")]
+}
