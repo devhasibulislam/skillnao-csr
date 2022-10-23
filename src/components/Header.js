9bdb1351fa5075ead977ab539f7fe87395e532c2
hasibulislam999 <hasib143sl@gmail.com>
2022-10-23 16:06:54 +0600

    header modification in progress

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,20 @@ import React from "react";
 import logo from "../../assets/header_logo.svg";
 
 const Header = () => {
-  return <section></section>;
+  return <section>
+    <div className="bg-[#3f876f]">
+        {/* logo */}
+        <div>
+            <img src={logo} alt="skill nao logo" />
+        </div>
+
+        {/* buttons */}
+        <div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+  </section>;
 };
 
 export default Header;
