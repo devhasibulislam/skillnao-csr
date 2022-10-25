d8ffeb0e7449dd5b83e58030c43d8e7bc7689785
hasibulislam999 <hasib143sl@gmail.com>
2022-10-25 22:24:45 +0600

    professional courses component modified

--- src/components/home/courses/ProfessionalCourses.js
+++ src/components/home/courses/ProfessionalCourses.js
@@ -1,56 +1,25 @@
-import React from 'react';
-import useGetCoursesWithQuery from '../../../utils/useGetCoursesWithQuery';
-import Highlight from '../Highlight';
-import Carousel from "react-multi-carousel";
-import Card from '../../Card';
-
+import React from "react";
+import useGetCoursesWithQuery from "../../../utils/useGetCoursesWithQuery";
+import Highlight from "../Highlight";
+import CourseSlider from "./CourseSlider";
 
 const ProfessionalCourses = () => {
-    const { courses, isLoading } = useGetCoursesWithQuery("category=professional");
+  const { courses, isLoading } = useGetCoursesWithQuery(
+    "category=professional"
+  );
 
-    if (isLoading) {
-      return <p>Loading...</p>;
-    }
-    const responsive = {
-        superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5,
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3,
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2,
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1,
-        },
-      };
-    return (
-        <div className="my-20 container mx-auto">
-        <h1 className="text-4xl text-center pb-12 font-bold">
-        প্রফেশনাল  <Highlight> সেকশান</Highlight>
-        </h1>
-        <Carousel
-          swipeable="true"
-          responsive={responsive}
-          autoPlay={true}
-          autoPlaySpeed={5000}
-          infinite={true}
-        >
-          {courses.map((course) => (
-            <div className='mx-4' key={course._id}>
-   <Card  course={course}></Card>
-            </div>
-         
-          ))}
-        </Carousel>
-      </div>
-    );
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className="my-20 container mx-auto">
+      <h1 className="text-4xl text-center pb-12 font-bold">
+        প্রফেশনাল <Highlight> সেকশান</Highlight>
+      </h1>
+      <CourseSlider courses={courses} />
+    </div>
+  );
 };
 
 export default ProfessionalCourses;
