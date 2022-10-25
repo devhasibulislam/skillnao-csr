047ec77ac6e1fe060edd6107d1cfc6e87e53658b
hasibulislam999 <hasib143sl@gmail.com>
2022-10-25 22:24:09 +0600

    academic course component modified

--- src/components/home/courses/AcademicCourses.js
+++ src/components/home/courses/AcademicCourses.js
@@ -1,55 +1,23 @@
-import React from 'react';
-import useGetCoursesWithQuery from '../../../utils/useGetCoursesWithQuery';
-import Card from '../../Card';
-import Highlight from '../Highlight';
-import Carousel from "react-multi-carousel";
+import React from "react";
+import useGetCoursesWithQuery from "../../../utils/useGetCoursesWithQuery";
+import Highlight from "../Highlight";
+import CourseSlider from "./CourseSlider";
 
 const AcademicCourses = () => {
-    const { courses, isLoading } = useGetCoursesWithQuery("category=academic");
+  const { courses, isLoading } = useGetCoursesWithQuery("category=academic");
 
-    if (isLoading) {
-      return <p>Loading...</p>;
-    }
-    const responsive = {
-        superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5,
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3,
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2,
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1,
-        },
-      };
-    return (
-        <div className="my-20 container mx-auto">
-        <h1 className="text-4xl text-center pb-12 font-bold">
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className="my-20 container mx-auto">
+      <h1 className="text-4xl text-center pb-12 font-bold">
         একাডেমিক <Highlight>সেকশান</Highlight>
-        </h1>
-        <Carousel
-          swipeable="true"
-          responsive={responsive}
-          autoPlay={true}
-          autoPlaySpeed={5000}
-          infinite={true}
-        >
-          {courses.map((course) => (
-            <div className='mx-4' key={course._id}>
-   <Card  course={course}></Card>
-            </div>
-         
-          ))}
-        </Carousel>
-      </div>
-    );
+      </h1>
+      <CourseSlider courses={courses} />
+    </div>
+  );
 };
 
 export default AcademicCourses;
