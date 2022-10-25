8d817b81d275b9c8cb24e49c637c76a0612f0f5a
hasibulislam999 <hasib143sl@gmail.com>
2022-10-25 14:54:41 +0600

    get all user utility modified

--- src/utils/useGetAllUsers.js
+++ src/utils/useGetAllUsers.js
@@ -8,10 +8,7 @@ function useGetAllUsers() {
       .then((res) => res.data);
 
   const { data, error } = useSWR(
-    [
-      "http://localhost:8080/user/all",
-      JSON.parse(localStorage.getItem("skillNaoToken")),
-    ],
+    ["http://localhost:8080/user/all", localStorage.getItem("skillNaoToken")],
     fetcher,
     { refreshInterval: 1000 }
   );
