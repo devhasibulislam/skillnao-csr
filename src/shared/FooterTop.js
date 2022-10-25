a5026f003b5ba23298827fd2922a4a61f9d9d449
Nipa <akternipa941@gmail.com>
2022-10-25 23:19:01 +0600

    footer change

--- src/shared/FooterTop.js
+++ src/shared/FooterTop.js
@@ -1,96 +1,81 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import footerLogo from "../assets/footer_logo.svg";
+
+import logo from "../assets/footer_logo.svg";
+import googlePlay from "../assets/home/icons/google_play.svg";
+import appleStore from "../assets/home/icons/app-store.svg";
+import facebook from "../assets/home/social/facebook.svg";
+import instagram from "../assets/home/social/instagram.svg";
+import twitter from "../assets/home/social/twitter.svg";
+import youtube from "../assets/home/social/youtube.svg";
 
 const FooterTop = () => {
   return (
-    <section>
-      <footer className="footer p-10 text-base-content">
+    <div>
+    <div className="container mx-auto text-white text-center lg:text-justify  pt-16">
+      <div className="flex flex-col lg:flex-row lg:justify-between">
         <div>
-          <Link to={"/home"}>
-            <img src={footerLogo} alt="skill nao logo" />
-          </Link>
-          <p>একাডেমিক ও প্রফেশনাল ক্যারিয়ারের ভার্চুয়াল মেন্টর</p>
-          <div className="grid grid-flow-col gap-4">
-            <a href="/">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-              </svg>
+          <img className="pb-4 mx-auto lg:mx-0" src={logo} alt="" />
+          <p className="pb-2">ইন্ডাস্ট্রি এবং একাডেমিক এক্সপার্টদের সঙ্গে </p>
+          <p className="pb-4">আলোচনার মাধ্যমে সঠিক ডিসিশন টি পছন্দ করুন</p>
+
+          <div className="text-2xl flex pb-12 justify-center lg:justify-start">
+            <a href="/" target={"_blank"} rel="noreferrer" className="mr-4">
+              <img src={facebook} alt="fb" />
             </a>
-            <a href="/">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-              </svg>
+            <a href="/" target={"_blank"} rel="noreferrer" className="mr-4">
+              <img src={instagram} alt="" />
             </a>
-            <a href="/">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-              </svg>
+            <a href="/" target={"_blank"} rel="noreferrer" className="mr-4">
+              <img src={twitter} alt="" />
+            </a>
+            <a href="/" target={"_blank"} rel="noreferrer">
+              <img src={youtube} alt="" />
             </a>
           </div>
         </div>
         <div>
-          <span className="footer-title">Services</span>
-          <Link to={"/"} className="link link-hover">
-            Branding
-          </Link>
-          <Link to={"/"} className="link link-hover">
-            Design
-          </Link>
-          <Link to={"/"} className="link link-hover">
-            Marketing
-          </Link>
-          <Link to={"/"} className="link link-hover">
-            Advertisement
-          </Link>
+          <h2 className="text-2xl pb-4">লিংক</h2>
+          <div className="flex flex-col gap-y-2">
+            <p>কোর্সসমূহ</p>
+            <p>উদ্যোক্তা</p>
+            <p>আমাদের সম্পর্কে</p>
+            <p>ক্যারিয়ার</p>
+          </div>
         </div>
         <div>
-          <span className="footer-title">Company</span>
-          <Link to={"/"} className="link link-hover">
-            About us
-          </Link>
-          <Link to={"/"} className="link link-hover">
-            Contact
-          </Link>
-          <Link to={"/"} className="link link-hover">
-            Jobs
-          </Link>
-          <Link to={"/"} className="link link-hover">
-            Press kit
-          </Link>
+          <h2 className="text-2xl pb-4">অন্যান্য</h2>
+          <div className="flex flex-col gap-y-2">
+            <p>অফারসমূহ</p>
+            <p>নোটস এবং গাইড</p>
+            <p>ব্লগ</p>
+            <p>ক্রাশ কোর্স</p>
+          </div>
         </div>
-        <div>
-          <span className="footer-title">Legal</span>
-          <Link to={"/"} className="link link-hover">
-            Terms of use
-          </Link>
-          <Link to={"/"} className="link link-hover">
-            Privacy policy
-          </Link>
-          <Link to={"/"} className="link link-hover">
-            Cookie policy
-          </Link>
+
+        <div className="">
+          <h2 className="text-2xl pb-4">যোগাযোগ </h2>
+          <p className="pb-2">
+            <i class="fa-solid fa-phone mr-2"></i> +৮৮০১৪৫৮৯৬৩{" "}
+          </p>
+          <p className="">
+            <i class="fa-solid fa-envelope mr-3"></i>skillnao03@gmail.com
+          </p>
         </div>
-      </footer>
-    </section>
+        <div className="flex flex-col justify-center lg:justify-start ">
+          <h2 className="text-2xl pb-4">ডাউনলোড করুন </h2>
+          <img
+            className="w-2/3 mx-auto lg:mx-0"
+            src={googlePlay}
+            alt="Google Play"
+          />
+          <img className="lg:ml-[-30px]" src={appleStore} alt="Apple Store" />
+        </div>
+      </div>
+
+    
+    
+    </div>
+  </div>
   );
 };
 
