f9e7d2dca929ad096df9bc08a48f48e59698d6a2
Nipa <akternipa941@gmail.com>
2022-10-25 20:37:45 +0600

    try to repush other branch

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,19 @@
 import React from "react";
+import Banner from "../components/home/Banner";
+import Count from "../components/home/Count";
+import Necessary from "../components/home/Necessary";
+import OurPartners from "../components/home/OurPartners";
+import Review from "../components/home/Review";
+import Solution from "../components/home/Solution";
 
 const Home = () => {
   return <section>
-    <p>This is Home page.</p>
+   <Banner/>
+   <Solution/>
+   <Count/>
+   <Necessary/>
+   <Review/>
+   <OurPartners/>
   </section>;
 };
 
