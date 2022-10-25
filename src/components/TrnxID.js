0a92de9e652736337e319e687a5cf37867cd0e72
hasibulislam999 <hasib143sl@gmail.com>
2022-10-25 14:53:58 +0600

    trnx id component modified

--- src/components/TrnxID.js
+++ src/components/TrnxID.js
@@ -1,9 +1,129 @@
+import axios from "axios";
 import React from "react";
+import toast from "react-hot-toast";
+import useGetUser from "../utils/useGetUser";
 
 const TrnxID = () => {
+  const { user, isLoading } = useGetUser();
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  async function handleTrnxID(event) {
+    event.preventDefault();
+
+    const trnxInfo = {
+      name: event.target.name.value,
+      email: event.target.email.value,
+      whatsApp: event.target.whatsApp.value,
+      trnxID: event.target.trnx.value,
+    };
+
+    // console.log(trnxInfo);
+
+    // const { data } = await axios.patch(
+    //   `http://localhost:8080/user/${id}`,
+    //   trnxInfo
+    // );
+
+    // console.log(data);
+
+    const updateUserWithTrnxID = async () => {
+      const request = await fetch(`http://localhost:8080/user/${user._id}`, {
+        method: "PATCH",
+        headers: {
+          "content-type": "application/json",
+          authorization: `Bearer ${localStorage.getItem("skillNaoToken")}`,
+        },
+        body: JSON.stringify(trnxInfo),
+      });
+      const response = await request.json();
+      console.log(response);
+      if (response.acknowledgement) {
+        toast.success("TrnxID accepted and store on DB.");
+        event.target.reset();
+      }
+    };
+    updateUserWithTrnxID();
+  }
+
   return (
     <section>
-      <p>From here, TrnxID conveyed.</p>
+      <h1 className="font-bold text-2xl text-center border-b-2 border-b-[#ffb96d] w-fit mx-auto mb-8">
+        Submit TrnxID
+      </h1>
+      <form onSubmit={handleTrnxID}>
+        {/* email input */}
+        <div className="flex justify-between items-center">
+          <label htmlFor="email">
+            ইমেইল অ্যাড্রেস <span className="text-red-500">*</span>
+          </label>
+          <input
+            type="email"
+            name="email"
+            value={user.email}
+            readOnly
+            className="input input-bordered input-success w-full max-w-xs"
+          />
+        </div>
+
+        <br />
+
+        {/* user name input */}
+        <div className="flex justify-between items-center">
+          <label htmlFor="name">
+            ইউজার নেম <span className="text-red-500">*</span>
+          </label>
+          <input
+            type="text"
+            name="name"
+            value={user.name}
+            readOnly
+            className="input input-bordered input-success w-full max-w-xs"
+          />
+        </div>
+
+        <br />
+
+        {/* whatsApp number input */}
+        <div className="flex justify-between items-center">
+          <label htmlFor="name">
+            হয়াটস-এপ নাম্বার <span className="text-red-500">*</span>
+          </label>
+          <input
+            type="text"
+            name="whatsApp"
+            value={user.whatsApp}
+            readOnly
+            className="input input-bordered input-success w-full max-w-xs"
+          />
+        </div>
+
+        <br />
+
+        {/* transaction ID */}
+        <div className="flex justify-between items-center">
+          <label htmlFor="password">
+            TrnxID <span className="text-red-500">*</span>
+          </label>
+          <input
+            type="text"
+            name="trnx"
+            className="input input-bordered input-success w-full max-w-xs"
+            required
+          />
+        </div>
+
+        {/* sign up button */}
+        <div className="text-right mt-6">
+          <input
+            type="submit"
+            className="btn btn-wide bg-[#006243] hover:bg-white hover:text-black border-0"
+            value="Submit"
+          />
+        </div>
+      </form>
     </section>
   );
 };
