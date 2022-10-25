51ed899ccc24b5f4aef0a4947cbfb9a79b1770f4
hasibulislam999 <hasib143sl@gmail.com>
2022-10-25 11:59:23 +0600

    professional route modified

--- src/routes/Professional.js
+++ src/routes/Professional.js
@@ -17,7 +17,7 @@ const Professional = () => {
         Total Courses:{" "}
         <span className="text-[#ffb96d] font-semibold">{courses?.length}</span>
       </h2>
-      <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 lg:gap-8 md:gap-6 gap-y-4">
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 lg:gap-8 md:gap-6 gap-y-4">
         {courses.map((course, index) => (
           <Card key={index} course={course} />
         )).reverse()}
