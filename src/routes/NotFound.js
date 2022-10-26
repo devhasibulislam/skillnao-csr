44c2e4a48aee8f1d8ee0dc564ce4dda6ad712b99
hasibulislam999 <hasib143sl@gmail.com>
2022-10-26 08:34:46 +0600

    not found route modified

--- src/routes/NotFound.js
+++ src/routes/NotFound.js
@@ -1,9 +1,20 @@
 import React from "react";
 
 const NotFound = () => {
-  return <section>
-    <p>404 | Not Found</p>
-  </section>;
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <p className="py-6">404</p>
+          <h1 className="text-5xl font-bold">Not Found!</h1>
+          <p className="py-6">
+            The requested page could not be found but may be available again in
+            the future.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
