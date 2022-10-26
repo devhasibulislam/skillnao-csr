aafa2f7af7b6c50203964fa36d4e4d0f4d6cfa1b
hasibulislam999 <hasib143sl@gmail.com>
2022-10-26 10:06:19 +0600

    bar color changed

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -49,7 +49,7 @@ const Header = () => {
               <label tabIndex={0} className="btn btn-ghost lg:hidden">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
+                  className="h-5 w-5 text-[#ffb663]"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
