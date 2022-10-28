59e7f3644dfc9f71f6b0ae92411f4d6f43e91642
hasibulislam999 <hasib143sl@gmail.com>
2022-10-28 23:59:10 +0600

    persist user fetching done

--- src/utils/useGetUser.js
+++ src/utils/useGetUser.js
@@ -4,22 +4,33 @@
  */
 
 import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 import useSWR from "swr";
 
 function useGetUser() {
+  const [user, setUser] = useState({});
+
   const fetcher = (url, token) =>
     axios
       .get(url, { headers: { authorization: `Bearer ${token}` } })
       .then((res) => res.data);
 
   const { data, error } = useSWR(
-    ["https://skillnao-ssr.onrender.com/user/me", localStorage?.getItem("skillNaoToken")],
+    [
+      "https://skillnao-ssr.onrender.com/user/me",
+      localStorage?.getItem("skillNaoToken"),
+    ],
     fetcher
   );
 
+  useEffect(() => {
+    setUser(data?.data);
+  }, [data?.data]);
+
   return {
-    user: data?.data,
-    isLoading: !error && !data?.data,
+    user: user,
+    isLoading: !error && !data,
     isError: error,
   };
 }
